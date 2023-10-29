622f35d283ec6edb147c1a31965a5b40d45c40c6
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2023-10-29 21:46:18 +0700

    First commit

---
+++ screens/SignupScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import React from "react";
+
+export default function SignupScreen() {
+    return (
+        <View>
+            <Text>SignupScreen</Text>
+            {/* <TextInput placeholder="Username" />
+            <TextInput placeholder="Password" />
+            <Button title="Login" onPress={() => alert("Login Pressed")} /> */}
+        </View>
+    );
+}
