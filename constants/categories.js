eb0e9165f058c4ff1debaf63131848302435cf0c
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2023-11-08 03:27:33 +0700

    Add categories

---
+++ constants/categories.js
@@ -0,0 +1,34 @@
+export const categories = [
+    {
+        id: 1,
+        title: "Tổng quan",
+    },
+    {
+        id: 2,
+        title: "Lịch sử",
+    },
+    {
+        id: 3,
+        title: "Kiến trúc",
+    },
+    {
+        id: 4,
+        title: "Ảnh",
+    },
+    {
+        id: 5,
+        title: "Video",
+    },
+    {
+        id: 6,
+        title: "Đặt vé",
+    },
+    {
+        id: 7,
+        title: "Đồ lưu niệm",
+    },
+    {
+        id: 8,
+        title: "Đánh giá",
+    },
+];
