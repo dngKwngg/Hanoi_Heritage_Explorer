e7a562eadf0f956435be0601634f0a01d3151789
VuThiThanhVinh-21020422 <21020422@vnu.edu.vn>
2023-11-12 17:36:40 +0700

    ImageScreen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TicketScreen from "./screens/TicketScreen";
 import { NativeWindStyleSheet } from "nativewind";
+import ImageScreen from "./screens/ImageScreen";
 const Stack = createNativeStackNavigator();
 
 NativeWindStyleSheet.setOutput({
@@ -14,34 +15,14 @@ function App() {
 		<NavigationContainer>
 			<Stack.Navigator
 				// initialRouteName="Splash
-				initialRouteName="Ticket"
+				initialRouteName="Image"
 				screenOptions={{ headerShown: false }}
 			>
 				<Stack.Screen name="Ticket" component={TicketScreen} />
+				<Stack.Screen name="Image" component={ImageScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
-
-	// return (
-	//     <View
-	//         style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-	//     >
-	//         <Text>Hello World!</Text>
-	//     </View>
-	// );
 }
 
 export default App;
-
-// import { StatusBar } from "expo-status-bar";
-// import React from "react";
-// import { Text, View } from "react-native";
-
-// export default function App() {
-// 	return (
-// 		<View className="flex-1 items-center justify-center bg-red-300">
-// 			<Text>Open up App.js to start working on your app!</Text>
-// 			<StatusBar style="auto" />
-// 		</View>
-// 	);
-// }
--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -3,48 +3,20 @@ import {
 	View,
 	TextInput,
 	Button,
-	Text,
-	TouchableOpacity,
+	Image,
 	StyleSheet,
+	Text,
+	ScrollView,
 	StatusBar,
-	ImageBackground,
-	Pressable,
-	Platform,
+	TouchableOpacity,
 	FlatList,
-} from "react-native";
+} from "react-native"; // Thêm StyleSheet
 import { Card } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
-import DateTimePicker from "@react-native-community/datetimepicker";
 import { categories } from "../constants/categories";
 import COLORS from "../constants/colors";
-const TicketScreen = () => {
-	const ticket_adult = 50000;
-	const ticket_child = 20000;
-	const [quantity_adult, setQuantity_adult] = useState(0);
-	const [quantity_child, setQuantity_child] = useState(0);
+const ImageScreen = () => {
 	const index = 0;
-	const increaseQuantity_adult = () => {
-		setQuantity_adult(quantity_adult + 1);
-	};
-
-	const decreaseQuantity_adult = () => {
-		if (quantity_adult > 0) {
-			setQuantity_adult(quantity_adult - 1);
-		}
-	};
-	const increaseQuantity_child = () => {
-		setQuantity_child(quantity_child + 1);
-	};
-
-	const decreaseQuantity_child = () => {
-		if (quantity_child > 0) {
-			setQuantity_child(quantity_child - 1);
-		}
-	};
-	const handleBookTicket = () => {
-		// Xử lý việc đặt vé ở đây
-		console.log("Đặt vé thành công!");
-	};
 	const destinations = [
 		{
 			name: "Hoàng Thành Thăng Long",
@@ -116,195 +88,14 @@ const TicketScreen = () => {
 		},
 	];
 	const [activeCategory, setActiveCategory] = useState(1);
-	const [dateOfBirth, setDateOfBirth] = useState("");
-	const [date, setDate] = useState(new Date());
-	const [showPicker, setShowPicker] = useState(false);
-	const toggleDatePicker = () => {
-		setShowPicker(!showPicker);
-	};
-	const onChange = ({ type }, selectedDate) => {
-		if (type == "set") {
-			const currentDate = selectedDate;
-			setDate(currentDate);
-			if (Platform.OS === "android") {
-				toggleDatePicker();
-				setDateOfBirth(currentDate.toDateString());
-			}
-		} else {
-			toggleDatePicker();
-		}
-	};
-	const renderTicket = (category) => {
+	const renderTicket = (item) => {
 		return (
-			<ImageBackground
-				source={{
-					uri: destinations[index].url,
-				}}
-				style={styles.background}
-				blurRadius={0.5}
-			>
-				<View
-					style={{ flex: 1, backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-				>
-					<StatusBar
-						barStyle="dark-content"
-						backgroundColor="#fff"
-						hidden={false}
-					/>
-
-					<View style={styles.form}>
-						<Card
-							containerStyle={{
-								borderRadius: 30,
-								width: "100%",
-							}}
-						>
-							<Text style={{ fontSize: 20 }}>
-								Vé vào cửa {destinations[index].name}{" "}
-							</Text>
-							<View
-								style={{ flexDirection: "row", marginTop: 10 }}
-							>
-								<Text style={{ fontSize: 18 }}>Ngày: </Text>
-
-								{showPicker && (
-									<DateTimePicker
-										mode="date"
-										display="spinner"
-										value={date}
-										onChange={onChange}
-									/>
-								)}
-								{!showPicker && (
-									<Pressable onPress={toggleDatePicker}>
-										<TextInput
-											style={styles.input}
-											placeholder="Chọn ngày đặt vé tham quan"
-											value={dateOfBirth}
-											onChangeText={setDateOfBirth}
-											placeholderTextColor="#11182744"
-											editable={false}
-										/>
-									</Pressable>
-								)}
-							</View>
-
-							<View style={[styles.container, { marginTop: 10 }]}>
-								<View style={{ flexDirection: "row" }}>
-									<Text style={styles.content}>
-										Người lớn:
-									</Text>
-									<Text
-										style={[
-											styles.content,
-											{ marginLeft: 7 },
-										]}
-									>
-										{ticket_adult} VND
-									</Text>
-								</View>
-								<View style={styles.quantityTicket}>
-									<TouchableOpacity
-										style={styles.quantityButton}
-										onPress={decreaseQuantity_adult}
-									>
-										<Text>-</Text>
-									</TouchableOpacity>
-
-									<Text style={styles.quantityText}>
-										{quantity_adult}
-									</Text>
-									<TouchableOpacity
-										style={styles.quantityButton}
-										onPress={increaseQuantity_adult}
-									>
-										<Text>+</Text>
-									</TouchableOpacity>
-								</View>
-							</View>
-
-							<View style={[styles.container, { marginTop: 10 }]}>
-								<View
-									style={{
-										flexDirection: "row",
-									}}
-								>
-									<Text style={styles.content}>Trẻ em:</Text>
-									<Text
-										style={[
-											styles.content,
-											{ marginLeft: 26 },
-										]}
-									>
-										{ticket_child} VND
-									</Text>
-								</View>
-								<View style={styles.quantityTicket}>
-									<TouchableOpacity
-										style={styles.quantityButton}
-										onPress={decreaseQuantity_child}
-									>
-										<Text>-</Text>
-									</TouchableOpacity>
-
-									<Text style={styles.quantityText}>
-										{quantity_child}
-									</Text>
-									<TouchableOpacity
-										style={styles.quantityButton}
-										onPress={increaseQuantity_child}
-									>
-										<Text>+</Text>
-									</TouchableOpacity>
-								</View>
-							</View>
-							<View
-								style={{
-									flexDirection: "row",
-									marginTop: 10,
-									marginBottom: 10,
-								}}
-							>
-								<Text
-									style={[
-										styles.content,
-										{ fontWeight: "bold" },
-									]}
-								>
-									Thành tiền:{" "}
-								</Text>
-								<Text
-									style={[
-										styles.content,
-										{ marginLeft: 133, fontWeight: "bold" },
-									]}
-								>
-									{quantity_adult * ticket_adult +
-										quantity_child * ticket_child}{" "}
-									VND
-								</Text>
-							</View>
-							<TouchableOpacity
-								style={styles.button}
-								onPress={handleBookTicket}
-							>
-								<Text
-									style={{
-										textAlign: "center",
-										color: "#fff",
-										fontSize: 18,
-										fontWeight: "bold",
-									}}
-								>
-									Đặt vé
-								</Text>
-							</TouchableOpacity>
-						</Card>
-					</View>
-				</View>
-			</ImageBackground>
+			<ScrollView style={{ marginTop: 30 }}>
+				{destinations[index].imageList.map(renderItems)}
+			</ScrollView>
 		);
 	};
+
 	const renderCategoryContent = () => {
 		const selectedCategory = categories.find(
 			(category) => category.id === activeCategory
@@ -313,8 +104,26 @@ const TicketScreen = () => {
 		if (!selectedCategory) {
 			return null;
 		}
-		if (selectedCategory.id === 6) {
+		if (selectedCategory.id === 4) {
 			return renderTicket(selectedCategory);
+		}
+	};
+	const renderItems = (item, idx) => {
+		if (idx % 2 === 0) {
+			return (
+				<View style={styles.row} key={idx}>
+					<Image
+						source={{ uri: item }}
+						style={styles.image}
+						key={idx}
+					/>
+					<Image
+						source={{ uri: destinations[index].imageList[idx + 1] }}
+						style={styles.image}
+						key={idx + 1}
+					/>
+				</View>
+			);
 		}
 	};
 	return (
@@ -398,56 +207,17 @@ const TicketScreen = () => {
 		</View>
 	);
 };
-
 const styles = StyleSheet.create({
-	background: {
+	image: {
 		flex: 1,
+		height: 200,
 		resizeMode: "cover",
-		justifyContent: "center",
-		marginTop: 4,
-	},
-	form: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		width: "100%",
-	},
-	container: {
-		flexDirection: "row",
-	},
-	content: {
-		fontSize: 16,
-	},
-	quantityTicket: {
-		flexDirection: "row",
-		marginLeft: 50,
-		width: 130,
-	},
-	button: {
-		backgroundColor: COLORS.primary,
-		paddingVertical: 10,
-		paddingHorizontal: 20,
 		borderRadius: 30,
+		margin: 4,
 	},
-	quantityButton: {
-		width: 25,
-		borderRadius: 5,
-		backgroundColor: "lightgray",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	quantityText: {
-		fontSize: 20,
-		paddingHorizontal: 10,
-		width: 80,
-		textAlign: "center",
-	},
-	input: {
-		color: "black",
-		fontSize: 16,
-		borderBottomWidth: 1,
-		borderBottomColor: "black",
+	row: {
+		flexDirection: "row",
+		marginBottom: 10,
 	},
 });
-
-export default TicketScreen;
+export default ImageScreen;
