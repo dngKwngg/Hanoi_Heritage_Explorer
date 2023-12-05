a900d24cf1733dfbc1c79346eee2a5fd99df8371
Nguyen Dang Quang <quangnguyenvk@gmail.com>
2023-12-06 05:10:19 +0700

    add shadow to button

--- components/AttractionTicket.js
+++ components/AttractionTicket.js
@@ -469,6 +469,15 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         marginRight: 10,
         borderRadius: 30,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+
+        elevation: 10,
     },
     increaseButton: {
         width: 30,
